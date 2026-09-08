45bcde69ccaaa6a84312a563672158fbce10fec0
Maite Arauco <gabriela.arauco.p@ucb.edu.bo>
2026-09-08 05:11:15 -0400

    feat: agregar boton para confirmar compra

--- src/presenter.js
+++ src/presenter.js
@@ -6,12 +6,21 @@ const codigoEstadoInput = document.querySelector("#codigo-estado");
 const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 const cancelarButton = document.querySelector("#cancelar-button");
+const confirmarButton = document.querySelector("#confirmar-button");
 let ultimoTotalCalculado = null;
 
 cancelarButton.addEventListener("click", () => {
   form.reset();
   div.innerHTML = "<p>" + "Compra cancelada" + "</p>";
   ultimoTotalCalculado = null;
+});
+
+confirmarButton.addEventListener("click", () => {
+  if (ultimoTotalCalculado === null) {
+    div.innerHTML = "<p>" + "Primero debes totalizar tu compra" + "</p>";
+    return;
+  }
+  div.innerHTML = "<p>Compra confirmada.Total: $" + ultimoTotalCalculado + "</p>";
 });
 
 form.addEventListener("submit", (event) => {
